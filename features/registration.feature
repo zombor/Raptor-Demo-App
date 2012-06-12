1f0192a36cef6156c22a381f1293f1286a55b057
Jeremy Bush <contractfrombelow@gmail.com>
2012-06-11 20:14:40 -0500

    Swap cuke account create paramaters to be more extendable

--- features/registration.feature
+++ features/registration.feature
@@ -3,7 +3,18 @@ Feature: Registration
   Scenario: User submits valid data for registration
     Given I am an unregistered user
     When I submit the following values for registration:
-      | email       | password |
-      | foo@bar.com | foobar   |
+      | email    | foo@bar.com |
+      | password | foobar      |
     Then I should have a valid user account
 
+  Scenario Outline: User submits invalid data for registration
+    Given I am an unregistered user
+    When I submit the following values for registration:
+      | email    | <email>    |
+      | password | <password> |
+    Then I should see a registration error for <error-type>
+
+    Examples:
+      | email       | password | error-type |
+      | foo@bar.com |          | password   |
+      |             | foobar   | email      |
--- features/step_definitions/registration.rb
+++ features/step_definitions/registration.rb
@@ -4,16 +4,27 @@ Given /^I am an unregistered user$/ do
 end
 
 When /^I submit the following values for registration:$/ do |table|
-  @email = table.raw[1][0]
-  @password = table.raw[1][1]
-  @result = MyKissList::Context::CreateUser.execute(
-    MyKissList::Records::User.new,
-    {'email' => @email, 'password' => @password}
-  )
+  @params = {}
+  table.raw.each do |key, value|
+    @params[key] = value
+  end
+
+  begin
+    @result = MyKissList::Context::CreateUser.execute(
+      MyKissList::Records::User.new,
+      @params
+    )
+  rescue Error::Validation => e
+    @exception = e
+  end
 end
 
 Then /^I should have a valid user account$/ do
   @result[:user].should be_a(MyKissList::Records::User)
-  @result[:user].email.should == @email
-  @result[:user].password.should == @password
+  @result[:user].email.should == @params['email']
+  @result[:user].password.should == @params['password']
+end
+
+Then /^I should see a registration error for (.+)$/ do |field|
+  @exception.errors[field.to_sym].should be_true
 end
